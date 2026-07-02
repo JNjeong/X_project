3f6cef39391095d6351d01eb9f338c94db03174e
JNjeong <wnsghd0902@gmail.com>
2026-07-02 15:15:38 +0900

    회원가입 api 완료

--- server/app.mjs
+++ server/app.mjs
@@ -1,11 +1,17 @@
 import express from "express"
 import {config} from "./config.mjs"
 import { connectDB } from "./db/database.mjs"
+import postsRouter from "./router/posts.mjs"
+import authRouter from "./router/auth.mjs"
+
 
 const app = express()
 app.use(express.json())
-
-
+app.use("/post", postsRouter)
+app.use("/auth", authRouter)
+app.use((req,res)=>{
+    res.sendStatus(404)
+})
 
 
 
--- server/router/auth.mjs
+++ server/router/auth.mjs
@@ -1,11 +1,15 @@
 import express from "express"
+import * as authController from "../controller/auth.mjs"
 
 const router = express.Router()
 
 // 회원가입
-
+router.post("/signup", authController.signup)
 // 로그인
+router.post("/login", authController.login)
 
 // 로그인 유지 체크
+router.post("/me", authController.me)
+
 
 export default router
